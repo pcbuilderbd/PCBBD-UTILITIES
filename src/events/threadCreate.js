95d9b1ee4ff3e3a159bd76835d95e1dc064ec46a
Nadid Wasique <76746289+DaTaklaCatto@users.noreply.github.com>
2023-10-28 17:13:45 +0600

    Forums v2

--- src/events/threadCreate.js
+++ src/events/threadCreate.js
@@ -2,50 +2,84 @@ const { ActionRowBuilder, ButtonBuilder, ChannelType } = require('discord.js');
 
 module.exports = {
   async execute(client) {
-    client.on("threadCreate", async (thread) => {
-      if (thread.type === ChannelType.PublicThread) {
-        if (thread.parentId === '1019653016120463400' || thread.parentId == '1154436432769851560') {
-          const actionRow = new ActionRowBuilder()
-            .addComponents(
-              new ButtonBuilder()
-                .setCustomId('pc')
-                .setLabel('Computer')
-                .setStyle('Danger'),
+    var newThreadid;
+    var row;
+    var newThreadOwnerId;
 
-              new ButtonBuilder()
-                .setCustomId('soft')
-                .setLabel('Software')
-                .setStyle('Secondary'),
+    client.on("threadCreate",async (newThread)=>{
+      if(newThread.type == ChannelType.PublicThread){
+        if (newThread.parentId == '1019653016120463400' || newThread.parentId == '1154436432769851560'){
+          const hardwareRoleButton = new ButtonBuilder()
+          .setCustomId('MentionHardwareRoleButton')
+          .setLabel(`Mention @${Object.keys(expertRoles)[0]}`)
+          .setStyle(ButtonStyle.Secondary)
 
-              new ButtonBuilder()
-                .setCustomId('hard')
-                .setLabel('Hardware')
-                .setStyle('Secondary')
-            );
+          const artsRoleButton = new ButtonBuilder()
+          .setCustomId('MentionArtsRoleButton')
+          .setLabel(`Mention @${Object.keys(expertRoles)[1]}`)
+          .setStyle(ButtonStyle.Secondary)
 
-          thread.send({
-            content: `Hello <@${thread.ownerId}>, please select one of the following to notify experts of that interest.`,
-            components: [actionRow],
-          }).then((message) => {
-            const collector = message.createMessageComponentCollector({ max: 1, time: 10000 });
+          const buildpcRoleButton = new ButtonBuilder()
+          .setCustomId('buildpcRoleButton')
+          .setLabel(`Build A PC`)
+          .setStyle(ButtonStyle.Danger)
 
-            collector.on('collect', async(interaction) => {
-              if (interaction.user.id === thread.ownerId && interaction.customId === 'pc') {
-                interaction.reply({ content: `Dont!`, ephemeral: true })
-                return
+          newThreadid = newThread.id
+          row = [new ActionRowBuilder().addComponents(hardwareRoleButton,artsRoleButton, buildpcRoleButton)]
+          newThreadOwnerId = newThread.ownerId
+
+          client.once("messageCreate", async (message)=>{
+            if(message.channelId == newThreadid){
+              await message.reply({
+                content: "Please click the required button to mention the type of experts or services you want to get help with!",
+                components: row,
+                ephemeral: true
               }
-              if (interaction.user.id === thread.ownerId && interaction.customId === 'soft') {
-                thread.send({ content: `<@&1154437163044307114> debug!` });
-                message.delete();
-              }
-              if (interaction.user.id === thread.ownerId && interaction.customId === 'hard') {
-                thread.send({ content: `<@&1154436920424804402>, assemble!` });
-                message.delete();
-              }
-            });
-          });
-        }
-      }
-    });
+            }
+
+     })
+    }
+   }
+
+})
+
+
+
+
+
+ client.on("interactionCreate",async (interaction)=>{
+  if(interaction.isButton() ){
+    if(interaction.customId == 'MentionHardwareRoleButton' && interaction.user.id == newThreadOwnerId){
+
+      await interaction.channel.send({
+        content:`<@&${'1154436920424804402'}>, this guy needs help!`,
+      }).then(()=>{
+        interaction.message.delete()
+      })
+    }else if(interaction.customId == 'MentionArtsRoleButton' && interaction.user.id == newThreadOwnerId){
+      
+      await interaction.channel.send({
+        content:`<@&${'1154437163044307114'}>, this guy needs help!`,
+        
+      }).then(()=>{
+        interaction.message.delete()
+      })  
+  }else if(interaction.customId == 'buildpcRoleButton' && interaction.user.id == newThreadOwnerId){
+    await interaction.channel.send({
+      content:`Please mention if you want to build with a monitor or not. Does it include any other peripherals like a keyboard, mouse, etc.? Also, don't forget to mention your budget and the purpose (gaming, editing etc.). The <@&${'1154436920424804402'}> gang is on their way!`,
+      
+    }).then(()=>{
+      interaction.message.delete()
+    })  
+
+  }else if(interaction.user.id != newThreadOwnerId){
+      await interaction.reply({
+        content:`You are not supposed to click this!`,
+        ephemeral: true
+      })
+      return
+    }
   }
-}
+ })
+
+})
