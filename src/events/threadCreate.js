5db007d3177e270516df3b9f7dedda052edda169
Nadid Wasique <76746289+DaTaklaCatto@users.noreply.github.com>
2023-10-28 16:08:18 +0600

    Updated build pc module

--- src/events/threadCreate.js
+++ src/events/threadCreate.js
@@ -31,8 +31,7 @@ module.exports = {
 
             collector.on('collect', (interaction) => {
               if (interaction.user.id === thread.ownerId && interaction.customId === 'pc') {
-                thread.send({ content: `You weren't supposed to click that lol.` })            
-                message.delete();
+                thread.send({ content: `You weren't supposed to click that lol.` })  
               }
               if (interaction.user.id === thread.ownerId && interaction.customId === 'soft') {
                 thread.send({ content: `<@&1154437163044307114> debug!` });
